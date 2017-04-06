493eea63eba7e0479d357bf3a9f59cf4becc5d44
MKS-Student <mks.student@hackreactor.com>
2017-04-05 18:46:56 -0700

    Pass SongQueue when a song is added test

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,17 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-  }
+    // Play the song if only one song is added
+    this.on('add', function() {
+      if (this.models.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+  },
+
+  playFirst: function() {
+    console.log('from playFirst: ', this);
+    this.models[0].play();
+  },
 
 });
